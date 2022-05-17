6c52f3acb7cd4f87cd0b5b60ae04c1acdcc7dd67
Romy Tsoupidi <tsoupidi@kth.se>
2022-05-17 10:41:10 +0200

    Add final scripts.

--- benchmarks/optimized/secunison/run.sh
+++ benchmarks/optimized/secunison/run.sh
@@ -4,9 +4,10 @@ pushd ../../../
 . secconenv
 popd
 
-for i in P{4..6}
+for i in P{9..9}
 do
     pushd $i
+    bash clean.sh
     bash -x run.sh &> out
     #bash -x run_only_chuffed.sh &> out_only_chuffed
     popd
