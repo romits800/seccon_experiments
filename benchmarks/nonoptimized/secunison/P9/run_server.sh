ad3b67b3871e28ffa2f7bd383c5894d94276fa93
Romy Tsoupidi <tsoupidi@kth.se>
2023-01-22 20:38:02 +0100

    Remove refs to name.

--- benchmarks/nonoptimized/secunison/P9/run_server.sh
+++ benchmarks/nonoptimized/secunison/P9/run_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH=/home/romi/.opam/4.10.0/bin:/home/romi/.local/bin:/home/romi/bin:/home/romi/.opam/4.10.0/bin:/home/romi/.cargo/bin:/home/romi/.wasmer/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/romi/bin/:/home/romi/repo/minizinc/MiniZincIDE-2.6.2-bundle-linux-x86_64/bin/:/home/romi/wasm/z3/z3-z3-4.8.10/build:/home/romi/wasm/solvers/cvc4/:/home/romi/wasm/solvers/yices2/yices-2.6.2//bin:/home/romi/wasm/solvers/boolector/boolector-3.2.1/build//bin:/home/romi/wasm/solvers/bitwuzla/Bitwuzla//bin:/home/romi/wasm/solvers/stp/stp/build/:/home/romi/.wasmer/globals/wapm_packages/.bin:/home/romi/.local/bin
 
 for arch in mips
 do
